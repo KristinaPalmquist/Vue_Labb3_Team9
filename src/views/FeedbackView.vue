0811fdb0cec329a3f9a5de3f6a4bac4a88ce82b9
Kristina_Palmquist <kristina.palmquist@gmail.com>
2023-02-22 14:22:46 +0100

    router works

---
+++ src/views/FeedbackView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="feedback">
+    <h1>Feedback</h1>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .feedback {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
