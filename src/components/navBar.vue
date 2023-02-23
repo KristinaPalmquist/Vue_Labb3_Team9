8290680560b5ec998b9e2bf54338d49b97ac7b1b
Lis Fredriksson <lisfredrikson@gmail.com>
2023-02-23 10:29:08 +0100

    nav updates

---
+++ src/components/navBar.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="">CINEMAP</RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="">Movies</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="">Cinemas</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="">Feedback</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <RouterView />
+</template>
