50effb4623ed2e285059c78e4b37a5acca717f42
Fetiamohammed <fetia-awol@hotmail.com>
2023-02-24 12:45:45 +0100

    change navBar

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-//import bootstarap
 import "bootstrap/dist/css/bootstrap.css";
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
-import "bootstrap/dist/css/bootstrap.css";
-
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js"
 import App from "./App.vue";
 import router from "./router";
 import axios from 'axios'
 import "./assets/base.css";
-
 const app = createApp(App);
 app.config.globalProperties.axios = axios
-
 app.use(createPinia());
 app.use(router);
 app.use(bootstrap);
