b9ccb97bc37577175cc76c51728350723a0ba6f0
Lis Fredriksson <lisfredrikson@gmail.com>
2023-02-27 18:59:03 +0100

    cinema search

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -18,23 +18,26 @@ export default {
             axios
              .get("movies.json")
              .then((response) => (
-                this.films = response.data,
-                console.log(response.data)
+                this.films = response.data
              ));
         },
         cinemaSearch() {
             axios
              .get("cinemas.json")
              .then((response) => (
-                this.cinemas = response.data,
-                console.log(response.data)
+                this.cinemas = response.data
              ));
         }
     },
     computed: {
-        searchFilter() {
+        searchFilterFilms() {
             return this.films.filter((data) => {
             return data.titleSweden.toLowerCase().includes(this.searchinput.toLowerCase());
+        });
+        },
+        searchFilterMovies() {
+            return this.cinemas.filter((data) => {
+            return data.name.toLowerCase().includes(this.searchinput.toLowerCase());
         });
         }
     }
@@ -44,16 +47,22 @@ export default {
 
 <template>
     <div class="input-group rounded">
-  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" v-model="searchinput"/>
+  <input type="search" class="form-control rounded" placeholder="Filmer och Biografer" aria-label="Search" aria-describedby="search-addon" v-model="searchinput"/>
   <span class="input-group-text border-0" id="search-addon">
     <img id="search-icon" src="../assets/img/search.png">
   </span>
 </div>
-<tr v-if="this.searchinput != ''"
+   <tr v-if="this.searchinput != ''"
     class="listdiv"
-    v-for="data in searchFilter"
+    v-for="data in searchFilterFilms"
     :key="data">
     <td>{{ data.titleSweden }}</td>
+    </tr>
+    <tr v-if="this.searchinput != ''"
+    class="listdiv"
+    v-for="data in searchFilterMovies"
+    :key="data">
+    <td>{{ data.name }}</td>
     </tr>
 </template>
 
