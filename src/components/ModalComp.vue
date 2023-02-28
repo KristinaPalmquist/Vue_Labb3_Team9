3af4fa749d01d072c2776880b6cf09b8dae74556
Fetiamohammed <fetia-awol@hotmail.com>
2023-03-01 00:02:24 +0100

    Feedback page
     I have made feedback form and it is responsive but the  css may need some changes.

---
+++ src/components/ModalComp.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="modal">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title">Hej {{ name }}!</h3>
+        <button class="close-button" @click="$emit('close')">X</button>
+      </div>
+      <div class="modal-body">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name"],
+};
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(251, 245, 245, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: black;
+  border-radius: 5px;
+  padding: 20px;
+  max-width: 300px;
+  max-height: auto;
+  justify-content: center;
+  color: white;
+}
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.close-button {
+  background-color: black;
+  color: white;
+}
+</style>
--- src/views/FeedbackView.vue
+++ src/views/FeedbackView.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="feedback">
-    <h1>Feedback</h1>
+  <div class="">
+    <FeedbackPage />
   </div>
 </template>
+<script>
+import FeedbackPage from "../components/FeedbackPage.vue";
+export default {
+  components: { FeedbackPage },
+};
+</script>
 
 <style scoped>
-@media (min-width: 1024px) {
-  .feedback {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+body {
+  width: auto;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  overflow-x: hidden;
 }
 </style>
