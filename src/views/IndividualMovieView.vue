2e588dba6c753177fd8d6e2ccfdffb17a73dc261
Kristina_Palmquist <kristina.palmquist@gmail.com>
2023-02-28 15:02:17 +0100

    individual movie view

--- src/views/IndividualMovieView.vue
+++ src/views/IndividualMovieView.vue
@@ -3,43 +3,6 @@ import axios from "axios";
 </script>
 
 <template>
-  <!-- <img class="poster" :src="`${movie.img}`" />
-  <h2>{{ movie.titleSweden }}</h2>
-  <p>Svensk premiär: {{ movie.premiere }}</p>
-  <a :href="`${movie.trailer}`">Se trailer för {{ movie.titleSweden }}</a>
-  <p>Längd: {{ movie.minutes }} minuter</p>
-  <p>Åldersgräns: {{ movie.ageLimit }} år</p>
-  <p>
-    Genre:
-    <template v-for="(genre, index) in movie.category" :key="index">
-      <template v-if="index > 0">, </template>
-      {{ genre }}
-    </template>
-  </p>
-  <p>Regissör: {{ movie.director }}</p>
-  <p>
-    Skådespelare:
-    <template v-for="(actor, index) in movie.actors" :key="index">
-      <template v-if="index > 0">, </template>
-      {{ actor }}
-    </template>
-  </p>
-  <p>{{ movie.plot }}</p>
-  <h3>Omdömen:</h3>
-  <p>
-    <template v-if="movie.imdb !== null">IMDb: {{ movie.imdb }}/10</template>
-  </p>
-  <p>
-    <template v-if="movie.rtTomatometer !== null"
-      >Rotten Tomatoes Tomatometer: {{ movie.rtTomatometer }}/100</template
-    >
-  </p>
-  <p>
-    <template v-if="movie.rtAudienceScore !== null"
-      >Rotten Tomatoes Audience Score: {{ movie.rtAudienceScore }}/100</template
-    >
-  </p> -->
-
   <div v-for="movie in movies" :key="movie.id">
     <img class="poster" :src="`${movie.img}`" />
     <h2>{{ movie.titleSweden }}</h2>
@@ -92,26 +55,14 @@ export default {
   data() {
     return {
       movies: [],
-      movie: {},
+      showMovie: {},
     };
   },
-  created() {
-    this.fetchMovies();
-  },
-  methods: {
-    fetchMovies() {
-      axios
-        .get(
-          "../../public/movies.json"
-          // , {
-          //   id: this.movieId,
-          // }
-        )
-        .then((movies) => {
-          this.movies = movies;
-          console.log("this Movies", this.movies);
-        });
-    },
+  mounted() {
+    axios.get("../../public/movies.json").then((response) => {
+      this.movies = response.data;
+      console.log("this Movies", this.movies);
+    });
   },
 };
 </script>
