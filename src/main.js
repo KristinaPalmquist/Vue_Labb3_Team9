29476b93ff0db05ec879647f632b080e74d5247b
Lis Fredriksson <lisfredrikson@gmail.com>
2023-03-02 14:20:38 +0100

    datepicker for navbar

--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
 import { createApp } from "vue";
-
-
-
-
-
 import { createPinia } from "pinia";
-import "bootstrap/dist/css/bootstrap.css";
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import moment from "moment/min/moment-with-locales";
 import "./assets/base.css";
+import "bootstrap/dist/css/bootstrap.css";
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
 const app = createApp(App);
 app.config.globalProperties.axios = axios;
 
 app.use(createPinia());
 app.use(router);
+app.component(VueDatePicker);
 app.use(bootstrap);
 app.use(moment);
 app.mount("#app");
