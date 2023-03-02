349720867b90f6f52d6a5f7d6d79762039b55c85
Lis Fredriksson <lisfredrikson@gmail.com>
2023-03-02 11:07:59 +0100

    cleaned

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import searchBar from "../components/searchBar.vue";
+import TestCard from "../components/TestCard2.vue";
 </script>
 
 <template>
   <searchBar />
+  <TestCard />
 </template>
 
 <style scoped></style>
--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,47 +1,12 @@
 <script setup>
-import axios from "axios";
 import TestCard2 from "../components/TestCard2.vue";
 // import useRouter from "vue-router";
 </script>
 
 <template>
-  <div class="movie">
-    <h2>{{ $route.params.id }}</h2>
-    <div v-for="movie in movies" :key="movie.id">
-      <input
-        type="button"
-        @click="onClick(movie.id)"
-        :value="`${movie.titleSweden}`"
-      />
-      <!-- <router-link to="/film/`${movie.id}`">{{
-        movie.titleSweden
-      }}</router-link>
-      <RouterView /> -->
-    </div>
-  </div>
   <TestCard2 />
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      movies: [],
-      id: Number,
-    };
-  },
-  created() {
-    this.fetchMovies();
-  },
-  methods: {
-    fetchMovies() {
-      axios.get("movies.json").then((movies) => (this.movies = movies.data));
-    },
-    onClick(id) {
-      this.$router.push("/film/" + id);
-    },
-  },
-};
-</script>
+<script></script>
 
 <style scoped></style>
