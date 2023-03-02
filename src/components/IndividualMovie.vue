8ac1eb0c2d99ee81c873dfaf2021e82976e80a39
Dimitri <dimpirogovp@hotmail.com>
2023-03-02 17:04:54 +0100

    adding missing movies in cinema.json

--- src/components/IndividualMovie.vue
+++ src/components/IndividualMovie.vue
@@ -3,59 +3,61 @@ import axios from "axios";
 </script>
 
 <template>
-  <div v-if="!movie">
-    Oj! Någonting har blivit fel, sidan för filmen du valt finns inte. Vänligen
-    gå tillbaka och välj en annan film.
-  </div>
-  <div v-if="movie">
-    <div v-for="movie in movies" :key="movie.id">
-      <img class="poster" :src="`${movie.img}`" />
-      <h2>{{ movie.titleSweden }}</h2>
-      <p>Svensk premiär: {{ movie.premiere }}</p>
-      <a :href="`${movie.trailer}`" target="_blank"
-        >Se trailer för {{ movie.titleSweden }} på YouTube</a
-      >
-      <p>Längd: {{ movie.minutes }} minuter</p>
-      <p>Åldersgräns: {{ movie.ageLimit }} år</p>
-      <p>
-        Genre:
-        <template v-for="(genre, index) in movie.category" :key="index">
-          <template v-if="index > 0">, </template>
-          {{ genre }}
-        </template>
-      </p>
-      <p>
-        Regissör:
-        <template v-for="(dir, index) in movie.director" :key="index">
-          <template v-if="index > 0">, </template>
-          {{ dir }}
-        </template>
-      </p>
-      <p>
-        Skådespelare:
-        <template v-for="(actor, index) in movie.actors" :key="index">
-          <template v-if="index > 0">, </template>
-          {{ actor }}
-        </template>
-      </p>
-      <p>{{ movie.plot }}</p>
-      <h5>Tillgängliga omdömen:</h5>
-      <p>
-        <template v-if="movie.imdb !== null"
-          >IMDb: {{ movie.imdb }}/10</template
+  <div class="container">
+    <div v-if="!movie">
+      Oj! Någonting har blivit fel, sidan för filmen du valt finns inte. Vänligen
+      gå tillbaka och välj en annan film.
+    </div>
+    <div v-if="movie">
+      <div v-for="movie in movies" :key="movie.id">
+        <img class="poster" :src="`${movie.img}`" />
+        <h2>{{ movie.titleSweden }}</h2>
+        <p>Svensk premiär: {{ movie.premiere }}</p>
+        <a :href="`${movie.trailer}`" target="_blank"
+          >Se trailer för {{ movie.titleSweden }} på YouTube</a
         >
-      </p>
-      <p>
-        <template v-if="movie.rtTomatometer !== null"
-          >Rotten Tomatoes Tomatometer: {{ movie.rtTomatometer }}/100</template
-        >
-      </p>
-      <p>
-        <template v-if="movie.rtAudienceScore !== null"
-          >Rotten Tomatoes Audience Score:
-          {{ movie.rtAudienceScore }}/100</template
-        >
-      </p>
+        <p>Längd: {{ movie.minutes }} minuter</p>
+        <p>Åldersgräns: {{ movie.ageLimit }} år</p>
+        <p>
+          Genre:
+          <template v-for="(genre, index) in movie.category" :key="index">
+            <template v-if="index > 0">, </template>
+            {{ genre }}
+          </template>
+        </p>
+        <p>
+          Regissör:
+          <template v-for="(dir, index) in movie.director" :key="index">
+            <template v-if="index > 0">, </template>
+            {{ dir }}
+          </template>
+        </p>
+        <p>
+          Skådespelare:
+          <template v-for="(actor, index) in movie.actors" :key="index">
+            <template v-if="index > 0">, </template>
+            {{ actor }}
+          </template>
+        </p>
+        <p>{{ movie.plot }}</p>
+        <h5>Tillgängliga omdömen:</h5>
+        <p>
+          <template v-if="movie.imdb !== null"
+            >IMDb: {{ movie.imdb }}/10</template
+          >
+        </p>
+        <p>
+          <template v-if="movie.rtTomatometer !== null"
+            >Rotten Tomatoes Tomatometer: {{ movie.rtTomatometer }}/100</template
+          >
+        </p>
+        <p>
+          <template v-if="movie.rtAudienceScore !== null"
+            >Rotten Tomatoes Audience Score:
+            {{ movie.rtAudienceScore }}/100</template
+          >
+        </p>
+      </div>
     </div>
   </div>
 </template>
