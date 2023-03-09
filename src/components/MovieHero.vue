08b83e930ccc347622817b4ef4fdde1b297728cf
Kristina_Palmquist <kristina.palmquist@gmail.com>
2023-03-09 17:06:15 +0100

    first movieHero layout draft

--- src/components/MovieHero.vue
+++ src/components/MovieHero.vue
@@ -9,55 +9,92 @@ import axios from "axios";
       tillbaka och välj en annan film.
     </div>
     <div v-if="movie">
-      <div v-for="movie in movies" :key="movie.id">
-        <img class="poster" :src="`${movie.img}`" />
-        <h2>{{ movie.titleSweden }}</h2>
-        <p>Svensk premiär: {{ movie.premiere }}</p>
-        <a :href="`${movie.trailer}`" target="_blank"
-          >Se trailer för {{ movie.titleSweden }} på YouTube</a
-        >
-        <p>Längd: {{ movie.minutes }} minuter</p>
-        <p>Åldersgräns: {{ movie.ageLimit }} år</p>
-        <p>
-          Genre:
-          <template v-for="(genre, index) in movie.category" :key="index">
-            <template v-if="index > 0">, </template>
-            {{ genre }}
-          </template>
-        </p>
-        <p>
-          Regissör:
-          <template v-for="(dir, index) in movie.director" :key="index">
-            <template v-if="index > 0">, </template>
-            {{ dir }}
-          </template>
-        </p>
-        <p>
-          Skådespelare:
-          <template v-for="(actor, index) in movie.actors" :key="index">
-            <template v-if="index > 0">, </template>
-            {{ actor }}
-          </template>
-        </p>
-        <p>{{ movie.plot }}</p>
-        <h5>Tillgängliga omdömen:</h5>
-        <p>
-          <template v-if="movie.imdb !== null"
-            >IMDb: {{ movie.imdb }}/10</template
-          >
-        </p>
-        <p>
-          <template v-if="movie.rtTomatometer !== null"
-            >Rotten Tomatoes Tomatometer:
-            {{ movie.rtTomatometer }}/100</template
-          >
-        </p>
-        <p>
-          <template v-if="movie.rtAudienceScore !== null"
-            >Rotten Tomatoes Audience Score:
-            {{ movie.rtAudienceScore }}/100</template
-          >
-        </p>
+      <div v-for="movie in movies" :key="movie.id" class="grid-container">
+        <div class="grid-item poster">
+          <img class="poster-image" :src="`${movie.img}`" />
+        </div>
+        <div class="grid-item title">
+          <h2>{{ movie.titleSweden }}</h2>
+        </div>
+        <div class="grid-item info">
+          <table id="info-table">
+            <tr>
+              <td class="info-title">Svensk premiär:</td>
+              <td>{{ movie.premiere }}</td>
+            </tr>
+            <tr>
+              <td class="info-title">Trailer på IMDb:</td>
+              <td>
+                <a :href="`${movie.trailer}`" target="_blank" id="trailer-link">
+                  {{ movie.titleSweden }}
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="info-title">Längd:</td>
+              <td>{{ movie.minutes }} minuter</td>
+            </tr>
+            <tr>
+              <td class="info-title">Åldersgräns:</td>
+              <td>{{ movie.ageLimit }} år</td>
+            </tr>
+            <tr>
+              <td class="info-title">Genre:</td>
+              <td>
+                <template v-for="(genre, index) in movie.category" :key="index">
+                  <template v-if="index > 0">, </template>
+                  {{ genre }}
+                </template>
+              </td>
+            </tr>
+            <tr>
+              <td class="info-title">Regissör:</td>
+              <td>
+                <template v-for="(dir, index) in movie.director" :key="index">
+                  <template v-if="index > 0">, </template>
+                  {{ dir }}
+                </template>
+              </td>
+            </tr>
+            <tr id="row-actors">
+              <td class="info-title">Skådespelare:</td>
+              <td>
+                <template v-for="(actor, index) in movie.actors" :key="index">
+                  <template v-if="index > 0">, </template>
+                  {{ actor }}
+                </template>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div class="grid-item text-area">
+          <p>{{ movie.plot }}</p>
+        </div>
+        <div class="grid-item rating">
+          <h5>Tillgängliga omdömen</h5>
+          <table id="rating-table">
+            <tr>
+              <td v-if="movie.imdb !== null" class="rating-site">IMDb:</td>
+              <td v-if="movie.imdb !== null">{{ movie.imdb }}/10</td>
+            </tr>
+            <tr>
+              <td v-if="movie.rtTomatometer !== null" class="rating-site">
+                Rotten Tomatoes Tomatometer:
+              </td>
+              <td v-if="movie.rtTomatometer !== null">
+                {{ movie.rtTomatometer }}/100
+              </td>
+            </tr>
+            <tr>
+              <td v-if="movie.rtAudienceScore !== null" class="rating-site">
+                Rotten Tomatoes Audience Score:
+              </td>
+              <td v-if="movie.rtAudienceScore !== null">
+                {{ movie.rtTomatometer }}/100
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -93,9 +130,144 @@ export default {
 </script>
 
 <style scoped>
+h2 {
+  font-size: 2.75rem;
+  color: var(--yellow);
+}
+
+h5 {
+  font-size: 1.375rem;
+  color: var(--yellow);
+}
+.container {
+  /* border-radius: 15px;
+  border: 1px solid var(--yellow); */
+  padding: 20px;
+}
+.grid-container {
+  display: grid;
+  grid-column: 100%;
+  grid-row: min-content min-content min-content min-content;
+  grid-template-areas:
+    "title"
+    "poster"
+    "text-area"
+    "info"
+    "rating";
+}
+
+.title {
+  grid-area: title;
+  /* border-radius: 15px;
+  border: 1px solid var(--yellow); */
+  padding: 20px;
+  width: 100%;
+  margin: auto;
+}
+.info {
+  grid-area: info;
+  /* border-radius: 15px;
+  border: 1px solid var(--yellow); */
+  padding: 10px;
+}
+
+.text-area {
+  grid-area: text-area;
+  /* border-radius: 15px;
+  border: 1px solid var(--yellow); */
+  padding: 10px;
+}
+
 .poster {
-  height: 600px;
-  max-height: 50vh;
-  /* max-width: 50vw; */
+  grid-area: poster;
+  /* border-radius: 15px;
+  border: 1px solid var(--yellow); */
+  padding: 20px;
+}
+
+.rating {
+  grid-area: rating;
+  border-radius: 15px;
+  border: 1px solid var(--yellow);
+  padding: 10px;
+}
+.poster-image {
+  max-width: 90vw;
+}
+
+/* What colors to use? */
+#trailer-link {
+  font-size: 1rem;
+  text-decoration: none;
+  color: var(--yellow);
+}
+#trailer-link:visited {
+  color: var(--red-dark);
+}
+
+table {
+  width: 100%;
+  font-size: 0.875rem;
+  /* background-color: var(--yellow-soft); */
+}
+
+tr {
+  height: 30px;
+}
+
+.info-title {
+  width: 30%;
+  /* background-color: var(--red-dark); */
+}
+
+.rating-site {
+  width: 60%;
+  /* background-color: var(--red-dark); */
+}
+
+#row-actors {
+  height: 60px;
+}
+
+#info-table {
+}
+
+#rating-table {
+}
+
+@media screen and (min-width: 1000px) {
+  .grid-container {
+    display: grid;
+    grid-column: auto 1fr 1fr 1fr auto;
+    grid-row: 1fr auto auto 1fr;
+    grid-template-areas:
+      ". title title poster ."
+      ". text-area text-area poster ."
+      ". info info poster ."
+      ". rating rating poster .";
+  }
+
+  /* .info {
+    grid-area: info;
+  }
+
+  .text-area {
+    grid-area: text-area;
+  }
+
+  .poster {
+    grid-area: poster;
+  }
+
+  .rating {
+    grid-area: rating;
+    border-radius: 15px;
+    border: 1px solid var(--yellow);
+    padding: 20px;
+  } */
+
+  .poster-image {
+    max-height: 100vh;
+  }
 }
 </style>
