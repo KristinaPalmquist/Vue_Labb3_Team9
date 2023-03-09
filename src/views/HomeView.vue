5252d7dd074cab95cd5fb59eeb0ca9f39866e69b
Kristina_Palmquist <kristina.palmquist@gmail.com>
2023-03-09 06:29:28 +0100

    adjustments to CSS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,18 +7,12 @@ import testComp from "../components/testComp.vue";
   <testComp @emit="receiveDate" :value="date" />
   <!-- remove after testing -->
   <div class="button-container">
-    <p>Button 1</p>
-    <input type="button" class="button btn1" value="Button 1" />
-    <p>Button 2</p>
-    <input type="button" class="button btn2" value="Button 2" />
-    <p>Button 3</p>
-    <input type="button" class="button btn3" value="Button 3" />
-    <p>Button 4</p>
-    <input type="button" class="button btn4" value="Button 4" />
-    <p>Button 5</p>
-    <input type="button" class="button btn5" value="Button 5" />
-    <p>Button 6</p>
-    <input type="button" class="button btn6" value="Button 6" />
+    <input type="button" class="btn1" value="Button 1" />
+    <input type="button" class="btn2" value="Button 2" />
+    <input type="button" class="btn3" value="Button 3" />
+    <input type="button" class="btn4" value="Button 4" />
+    <input type="button" class="btn5" value="Button 5" />
+    <input type="button" class="btn6" value="Button 6" />
   </div>
   <!-- end of button-test -->
   <MoviesList />
