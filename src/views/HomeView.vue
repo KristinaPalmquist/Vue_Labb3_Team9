cb310958b491ed285cbe254e79c629c7cc412541
Kristina_Palmquist <kristina.palmquist@gmail.com>
2023-03-09 11:09:55 +0100

    css updates from stakeholder meeting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,8 @@ import testComp from "../components/testComp.vue";
   <testComp @emit="receiveDate" :value="date" />
   <!-- remove after testing -->
   <div class="button-container">
-    <input type="button" class="btn1" value="Button 1" />
-    <input type="button" class="btn2" value="Button 2" />
-    <input type="button" class="btn3" value="Button 3" />
-    <input type="button" class="btn4" value="Button 4" />
-    <input type="button" class="btn5" value="Button 5" />
-    <input type="button" class="btn6" value="Button 6" />
+    <input type="button" class="btn-light" value="Button 5" />
+    <input type="button" class="btn-dark" value="Button 6" />
   </div>
   <!-- end of button-test -->
   <MoviesList />
