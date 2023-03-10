eb4c1caa03d6a7a5d199ef11f74d6ce065ead9b8
Kristina_Palmquist <kristina.palmquist@gmail.com>
2023-03-10 15:38:05 +0100

    change to CSS code for color

--- src/main.js
+++ src/main.js
@@ -5,21 +5,21 @@ import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
-import store from './store'
+import store from "./store";
 import moment from "moment/min/moment-with-locales";
 import "./assets/base.css";
 import VueDatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
-import vClickOutside from "click-outside-vue3"
+import vClickOutside from "click-outside-vue3";
 
 const app = createApp(App);
 app.config.globalProperties.axios = axios;
 
 app.use(createPinia());
 app.use(router);
-app.use(vClickOutside)
+app.use(vClickOutside);
 app.component(VueDatePicker);
 app.use(bootstrap);
 app.use(moment);
-app.use(store)
+app.use(store);
 app.mount("#app");
